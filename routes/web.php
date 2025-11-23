f848cd8bcb3a2ad85f04b5dd783c65fac02bfb5b
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-11-23 14:00:14 +0700

    tambah route home '/'

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\GaleriController;
 
+//home
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
 Route::get('/login', [LoginController::class, 'login'])
     ->name('login')
     ->middleware('guest');
