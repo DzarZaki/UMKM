69fca361d96c66d7f0055adcd0896c7dd9130b35
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-12-18 23:29:35 +0700

    perbaikan sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,6 @@
     @include('partials.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-    
     <script src="{{ asset('js/home.js') }}"></script>
     <script>
 document.getElementById('waForm').addEventListener('submit', function(e) {
