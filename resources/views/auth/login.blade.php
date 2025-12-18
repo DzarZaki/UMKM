98fb4336f8bd72260409855178bffd6158bb6aa1
enrexl <ersa.aditya.ft22@mail.umy.ac.id>
2025-12-18 21:32:14 +0700

    memperbaiki login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -26,70 +26,64 @@
   </head>
 
   <body class="bg-gradient-primary">
-    <div class="container">
-      <!-- Outer Row -->
-      <div class="row justify-content-center">
-        <div class="col-xl-10 col-lg-12 col-md-9">
-          <div class="card o-hidden border-0 shadow-lg my-5">
-            <div class="card-body p-0">
-              <!-- Nested Row within Card Body -->
-              <div class="row">
-                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+    <div class="container d-flex align-items-center justify-content-center" style="min-height: 100vh">
+  <!-- Outer Row -->
+  <div class="row w-100 justify-content-center">
+    <div class="col-lg-6 col-md-8 col-sm-10">
+      <div class="card o-hidden border-0 shadow-lg">
+        <div class="card-body p-5">
 
-                <div class="col-lg-6">
-                  <div class="p-5">
-                    <div class="text-center">
-                      <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
-                    </div>
-
-                    <form
-                      class="user"
-                      method="POST"
-                      action="{{ route('login') }}"
-                    >
-                      @csrf
-
-                      <div class="form-group">
-                        <input
-                          type="text"
-                          name="username"
-                          class="form-control form-control-user"
-                          value="{{ old('username') }}"
-                          placeholder="Username"
-                          required
-                          autofocus
-                        />
-                      </div>
-
-                      <div class="form-group">
-                        <input
-                          type="password"
-                          name="password"
-                          class="form-control form-control-user"
-                          placeholder="Password"
-                          required
-                        />
-                      </div>
-
-                      @error('username')
-                      <div class="text-danger small mb-2">{{ $message }}</div>
-                      @enderror
-
-                      <button
-                        type="submit"
-                        class="btn btn-primary btn-user btn-block"
-                      >
-                        Login
-                      </button>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
+          <!-- Center content -->
+          <div class="text-center mb-4">
+            <h1 class="h4 text-gray-900">Welcome Back!</h1>
           </div>
+
+          <form
+            class="user"
+            method="POST"
+            action="{{ route('login') }}"
+          >
+            @csrf
+
+            <div class="form-group">
+              <input
+                type="text"
+                name="username"
+                class="form-control form-control-user"
+                value="{{ old('username') }}"
+                placeholder="Username"
+                required
+                autofocus
+              />
+            </div>
+
+            <div class="form-group">
+              <input
+                type="password"
+                name="password"
+                class="form-control form-control-user"
+                placeholder="Password"
+                required
+              />
+            </div>
+
+            @error('username')
+              <div class="text-danger small mb-2">{{ $message }}</div>
+            @enderror
+
+            <button
+              type="submit"
+              class="btn btn-primary btn-user btn-block"
+            >
+              Login
+            </button>
+          </form>
+
         </div>
       </div>
     </div>
+  </div>
+</div>
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
